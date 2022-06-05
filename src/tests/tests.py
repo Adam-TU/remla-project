760d8af8eb3c80915d37315cc332229322d21306
Pepijn te Marvelde <p.h.temarvelde@student.tudelft.nl>
2022-06-05 16:25:40 +0200

    fix test

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,4 +1,5 @@
 """Module to test ML pipeline logic"""
+import os
 import unittest
 
 from src.features.build_features import (
@@ -45,6 +46,7 @@ class TestPipeLine(unittest.TestCase):
         self.assertEqual(tfidf_reversed_vocab[4516], expected_tag)
 
     def test_inference_api(self):
+        os.environ["PROMETHEUS_MULTIPROC_DIR"] = "/tmp/prom" # nosec
         tester = app.test_client()
         response = tester.post("/predict", json={"title": "this is a python title"})
         self.assertIn(b"python", response.data)
