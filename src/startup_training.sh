9b3a7bd911f34736559b3c99b355fb8c86bbcc4c
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-06-20 11:58:50 +0200

    some initial dockerfile changes traning service

--- src/startup_training.sh
+++ src/startup_training.sh
@@ -3,7 +3,7 @@
 # load google drive api key secret into file for use by DVC
 python src/training_service/load_key.py
 
-dvc init --no-scm -f
+dvc init
 
 # create config entries for gdrive authentication to go automatically 
 # by utilizing the API_KEY_SECRET json credentials
