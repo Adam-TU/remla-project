bad4ddc7801e6040d159152b793d92e912e56ec4
ptemarvelde <45282601+ptemarvelde@users.noreply.github.com>
2022-06-21 23:08:07 +0200

    Webservice in config (#10)
    * Init stackoverflow scraper
    
    * result file of from using the api now contains time
    
    * Tensorflow data validation
    
    * Fix TFDV (not Tfidf)
    
    * Added web frontend working on interconnection with model
    
    * Added mylib and jars for web frontend
    
    * create bash script for building images for web ui
    
    * fix format
    
    * Web frontend can retrieve tags from model
    
    Co-authored-by: Kevin Nanhekhan <Kevin-RN@users.noreply.github.com>
    
    * Changed folder structure to have web frontend in src
    
    * Changed Dockerfile to package web frontend
    
    * Dockerize web application
    
    * create frontend k8s yaml and move to correct dockerfile
    
    * remove redundant docker-compose.yml
    
    * Working on config for webservice Kubernetes
    
    Co-authored-by: Kevin Nanhekhan <Kevin-RN@users.noreply.github.com>
    
    * Working on config for webservice in Kubernetes
    
    * Working on config frontend
    
    * moved inference frontend to own yaml
    
    Co-authored-by: Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
    Co-authored-by: Adam <a.a.kadiev@student.tudelft.nl>
    Co-authored-by: Kevin Nanhekhan <Kevin-RN@users.noreply.github.com>
    Co-authored-by: Adam <kadiev.a.adam@gmail.com>

---
+++ src/data/data_validation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import tensorflow_data_validation as tfdv
+from tensorflow_data_validation.utils.display_util import get_anomalies_dataframe
+
+
+def validate(filename):
+    df_train = pd.read_csv("../../data/raw/train.tsv", sep="\t")
+    df_scraped = pd.read_csv("../../data/external/" + filename, sep="\t")
+    # df_scraped = pd.read_csv("../../data/external/result_1654101025.tsv", sep='\t')
+    # df_scraped = df_scraped.drop(['title'], axis=1)
+    train_stats = tfdv.generate_statistics_from_dataframe(df_train)
+    test_stats = tfdv.generate_statistics_from_dataframe(df_scraped)
+
+    schema = tfdv.infer_schema(train_stats)
+    anomalies = tfdv.validate_statistics(statistics=test_stats, schema=schema)
+
+    df_anomalies = get_anomalies_dataframe(anomalies)
+
+    if df_anomalies.empty:
+        print("No anomalies found")
+    else:
+        for index, row in df_anomalies.iterrows():
+            print(f"Feature: {index} has anomaly: '{row[1]}'")
