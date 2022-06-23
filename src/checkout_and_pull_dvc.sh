bc2ae9ba1a1c670e631318ded5dc5fd1bb6c924e
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-06-23 02:25:47 +0200

    added git fetch for inference-service to get the latest commit tree. Otherwise, changing to a new commit will not work.

--- src/checkout_and_pull_dvc.sh
+++ src/checkout_and_pull_dvc.sh
@@ -1,5 +1,7 @@
 cd /root/dvc-versioning
 
+git fetch
+
 # checkout a commit in branch dvc-versioning and only get dvc.lock (DO NOT CHECKOUT OHTER BRANCHES)
 # the commit must contain valid dvc.lock file 
 git checkout $CHECKOUT_COMMIT_HASH -- dvc.lock
